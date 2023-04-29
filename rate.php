c8ddd941be1b0f64346f14c639348e20a1c53675
skynettw <skynet.tw@gmail.com>
2023-04-29 16:43:08 +0800

    add  array

--- rate.php
+++ rate.php
@@ -1,3 +1,9 @@
+<?php
+   $currency = array("美金"=>30.4, 
+                     "港幣"=>3.76,
+                     "英鎊"=>37.5,
+                     "澳幣"=>19.93);
+?>
 <!doctype html>
 <html>
   <head>
@@ -10,7 +16,15 @@
         <?php include "menu.php"; ?>
         <hr>
         <form action="rate.php" method="POST">
-        美金：<input type=text size=10 name=usd required>元 
+        幣別：
+        <select name="currency">
+        <?php 
+            foreach(array_keys($currency) as $curr) {
+                echo "<option value='$curr'>$curr</option>";
+            }
+        ?>
+        </select>
+        <input type=text size=10 name=usd required>元 
         <input type=submit value="開始換算">
         </form>
 
