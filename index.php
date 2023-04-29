b3e4e94d7dc3a8692080583c89134b50f7402c7c
skynettw <skynet.tw@gmail.com>
2023-04-29 14:41:26 +0800

    add php codes for generating the date selection

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <h2>Welcome to my website!!</h2>
 <hr>
 <?php
-    for($i=1; $i<=6; $i++) {
-        echo "<h$i>Hello</h$i>";
-    }
+    //for($i=1; $i<=6; $i++) {
+    //    echo "<h$i>Hello</h$i>";
+    //}
 ?>
+民國
+<select>
+    <?php
+        for($i=1900; $i<=2023; $i++) {
+            echo "<option value='$i'>$i</option>";
+        }
+        
+    ?>
+</select>
+年
+月
+日
