f9b498fd7f010d80336e821fd707cc32bc5456b2
skynettw <skynet.tw@gmail.com>
2023-04-29 15:39:59 +0800

    add lotto.php menu item

---
+++ lotto.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <title>何敏煌的網站</title>
+    <?php include "blink.php"; ?>
+  </head>
+  <body>
+    <div class="container">
+        <h2>樂透預測</h2>
+        <?php include "menu.php"; ?>
+        <hr>
+        
+    </div>
+    <?php include "bscript.php"; ?>
+  </body>
+</html>
--- menu.php
+++ menu.php
@@ -1,26 +1,26 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">何敏煌</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="https://ccet.nkust.edu.tw/">不分系</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="/phpmyadmin/">資料庫管理</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            我的作業
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="lotto.php">樂透預測</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
