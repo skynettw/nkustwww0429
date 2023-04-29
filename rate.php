ffb7f0ad95b6503dba29eae83268221fe0818c62
skynettw <skynet.tw@gmail.com>
2023-04-29 16:17:50 +0800

    finish usd to ntd converter

--- rate.php
+++ rate.php
@@ -6,11 +6,22 @@
   </head>
   <body>
     <div class="container">
-        <h2>匯率換算</h2>
+        <h2>台幣換算</h2>
         <?php include "menu.php"; ?>
         <hr>
+        <form action="rate.php" method="POST">
+        美金：<input type=text size=10 name=usd required>元 
+        <input type=submit value="開始換算">
+        </form>
+
         <?php
-            
+            $usd = $_POST["usd"];
+            if ($usd!="") {
+                $ntd = $usd * 30.34;
+                echo "美金" . 
+                    $usd . "元等於台幣" . 
+                    $ntd . "元";
+            }
         ?>
     </div>
     <?php include "bscript.php"; ?>
